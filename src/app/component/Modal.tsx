4bf932b56036e0ace7226ccd178ffc23239f82a9
Camilla-Barb <barbiericamilla99@gmail.com>
2025-02-24 12:38:24 +0100

    FocusTrap library & overflow hidden when modal is open

--- src/app/component/Modal.tsx
+++ src/app/component/Modal.tsx
@@ -3,6 +3,7 @@
 import { XMarkIcon } from "@heroicons/react/16/solid";
 import { motion } from "framer-motion";
 import { useEffect, useRef } from "react";
+import { FocusTrap } from "focus-trap-react";
 
 interface ModalProps {
   isOpen: boolean;
@@ -23,56 +24,61 @@ const Modal = ({
 
   useEffect(() => {
     if (isOpen) {
+      document.body.classList.add("overflow-hidden");
       setTimeout(() => {
         closeBtn.current?.focus();
       }, 100);
+    } else {
+      document.body.classList.remove("overflow-hidden");
     }
   }, [isOpen]);
 
   if (!isOpen) return null;
 
   return (
-    <div
-      onKeyDown={(e) => {
-        if (e.key === "Escape") {
-          onClose();
-        }
-      }}
-      tabIndex={-1}
-      className="modal-open fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 overflow-hidden"
-    >
-      <motion.div
-        className="bg-white h-screen top-0 left-0 p-6 rounded-lg shadow-lg max-w-lg w-full relative"
-        role="dialog"
-        aria-modal="true"
-        aria-labelledby={title && "modal-title"}
-        aria-describedby={description && "modal-description"}
-        initial={{ opacity: 0, scale: 0.95 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 0.4 }}
-        exit={{ opacity: 0, scale: 0.95 }}
+    <FocusTrap active={isOpen}>
+      <div
+        onKeyDown={(e) => {
+          if (e.key === "Escape") {
+            onClose();
+          }
+        }}
+        tabIndex={-1}
+        className="modal-open fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 overflow-hidden"
       >
-        <button
-          className="w-8 absolute top-4 right-4 z-10"
-          ref={closeBtn}
-          onClick={onClose}
-          aria-label="Close"
+        <motion.div
+          className="bg-white h-screen top-0 left-0 p-6 rounded-lg shadow-lg max-w-lg w-full relative"
+          role="dialog"
+          aria-modal="true"
+          aria-labelledby={title && "modal-title"}
+          aria-describedby={description && "modal-description"}
+          initial={{ opacity: 0, scale: 0.95 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.4 }}
+          exit={{ opacity: 0, scale: 0.95 }}
         >
-          <XMarkIcon />
-        </button>
-        {title && (
-          <h2 className="sr-only" id="modal-title">
-            {title}
-          </h2>
-        )}
-        {description && (
-          <p className="sr-only" id="modal-description">
-            {description}
-          </p>
-        )}
-        {children}
-      </motion.div>
-    </div>
+          <button
+            className="w-8 absolute top-4 right-4 z-10"
+            ref={closeBtn}
+            onClick={onClose}
+            aria-label="Close"
+          >
+            <XMarkIcon />
+          </button>
+          {title && (
+            <h2 className="sr-only" id="modal-title">
+              {title}
+            </h2>
+          )}
+          {description && (
+            <p className="sr-only" id="modal-description">
+              {description}
+            </p>
+          )}
+          {children}
+        </motion.div>
+      </div>
+    </FocusTrap>
   );
 };
 
