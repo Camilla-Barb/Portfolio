85b55d56ac05229f592066248e5d373eb4eb9ed0
Camilla-Barb <barbiericamilla99@gmail.com>
2025-02-26 20:54:28 +0100

    About page - Pie chart image

--- src/app/component/Prefooter.tsx
+++ src/app/component/Prefooter.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Prefooter() {
   return (
-    <section className="w-full mt-10 bg-yellow-900/10 flex justify-end items-center p-2">
+    <section className="w-full mt-10 bg-yellow-900/10 flex justify-end items-center p-2 lg:p-4">
       <article className="pr-4 font-bold">
         <Link href="/pages/contact">Contact me</Link>
       </article>
--- src/app/pages/about/page.tsx
+++ src/app/pages/about/page.tsx
@@ -1,8 +1,19 @@
+import Image from "next/image";
+
 export default function AboutPage() {
   return (
-    <main className="p-10">
+    <section className="prefooter-section p-5 border-t border-yellow-950/30">
       <h1 className="text-3xl font-bold">About Me</h1>
-      <p>Benvenuto nella mia pagina About!</p>
-    </main>
+
+      <Image
+        src="/img/pie-chart.png"
+        width={850}
+        height={850}
+        className="bg-center bg-no-repeat"
+        alt=""
+        priority
+        aria-hidden
+      />
+    </section>
   );
 }
